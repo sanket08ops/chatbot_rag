63f286e26abdc4898570c4208f9f65e491f8a736
sanket08ops <sanketsaraf08@gmail.com>
2025-04-13 15:21:44 +0530

    Update session_5_rag_streamlit.py

--- session_5_rag_streamlit.py
+++ session_5_rag_streamlit.py
@@ -60,7 +60,6 @@ api_key = os.getenv("GOOGLE_API_KEY")
 
 import streamlit as st
 import os
-#import tempfile
 from session_4_rag_backend import (
     setup_api_key,
     upload_pdf,
@@ -70,7 +69,7 @@ from session_4_rag_backend import (
     embed_documents,
     store_embeddings,
     get_context_from_chunks,
-    query_with_full_context
+    query_with_full_contex
 )
 
 # Page configuration
